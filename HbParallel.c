2bbece607c708af24e2d0561401271409d784f2f
Triang3l <triang3l@yandex.ru>
2019-02-08 15:54:05 +0300

    HbParallel: Thread and locks on Windows

---
+++ HbParallel.c
@@ -0,0 +1,62 @@
+#include "HbParallel.h"
+
+#if defined(HbPlatform_OS_Windows)
+#include "HbText.h"
+#include <process.h>
+#include <Windows.h>
+
+typedef struct HbParalleli_Windows_Thread_Parameters {
+	HbParallel_Thread_Entry entry;
+	void * data;
+	char name[HbParallel_Thread_MaxNameLength + 1];
+} HbParalleli_Windows_Thread_Parameters;
+
+#pragma pack(push, 8)
+typedef struct HbParalleli_Windows_Thread_NameInfo {
+	DWORD type;
+	char const * name;
+	DWORD threadID;
+	DWORD flags;
+} HbParalleli_Windows_Thread_NameInfo;
+#pragma pack(pop)
+
+static void HbParalleli_Windows_Thread_Entry(void * parametersPointer) {
+	HbParalleli_Windows_Thread_Parameters parameters =
+			*((HbParalleli_Windows_Thread_Parameters const *) parametersPointer);
+	free(parametersPointer);
+	if (parameters.name[0] != '\0') {
+		HbParalleli_Windows_Thread_NameInfo nameInfo = {
+			.type = 0x1000,
+			.name = parameters.name,
+			.threadID = (DWORD) -1,
+			.flags = 0,
+		};
+		__try {
+			RaiseException(0x406d1388, 0, sizeof(nameInfo) / sizeof(ULONG_PTR), (ULONG_PTR const *) &nameInfo);
+		} __except (EXCEPTION_EXECUTE_HANDLER) {}
+	}
+	parameters.entry(parameters.data);
+}
+
+HbBool HbParallel_Thread_Start(HbParallel_Thread * thread, char const * name, HbParallel_Thread_Entry entry, void * data) {
+	HbParalleli_Windows_Thread_Parameters * parameters = malloc(sizeof(HbParalleli_Windows_Thread_Parameters));
+	if (parameters == HbNull) {
+		return HbFalse;
+	}
+	parameters->entry = entry;
+	parameters->data = data;
+	if (name != HbNull) {
+		HbTextA_Copy(parameters->name, HbArrayLength(parameters->name), name);
+	} else {
+		parameters->name[0] = '\0';
+	}
+	uintptr_t handle = _beginthread(HbParalleli_Windows_Thread_Entry, 0, parameters);
+	if (handle == -1L) {
+		free(parameters);
+		return HbFalse;
+	}
+	*thread = (HANDLE) handle;
+	return HbTrue;
+}
+
+#endif
---
+++ HbParallel.h
@@ -0,0 +1,56 @@
+#ifndef HbInclude_HbParallel
+#define HbInclude_HbParallel
+#include "HbCommon.h"
+
+// Note: acquire is used like "check atomic -> acquire -> read dependencies",
+// release is used like "write dependencies -> release -> update atomic".
+
+#define HbParallel_Thread_MaxNameLength 15
+
+#if defined(HbPlatform_OS_Windows)
+/**************************************************
+ * Windows and Visual C parallelization primitives
+ **************************************************/
+#include <intrin.h>
+#include <Windows.h>
+
+typedef CRITICAL_SECTION HbParallel_Mutex;
+HbForceInline HbParallel_Mutex_Init(HbParallel_Mutex * mutex) {
+	InitializeCriticalSection(mutex);
+	return HbTrue;
+}
+#define HbParallel_Mutex_Destroy DeleteCriticalSection
+#define HbParallel_Mutex_Lock EnterCriticalSection
+#define HbParallel_Mutex_Unlock LeaveCriticalSection
+
+typedef SRWLOCK HbParallel_RWLock;
+HbForceInline HbParallel_RWLock_Init(HbParallel_RWLock * lock) {
+	InitializeSRWLock(lock);
+	return HbTrue;
+}
+#define HbParallel_RWLock_Destroy(lock) {}
+#define HbParallel_RWLock_LockRead AcquireSRWLockShared
+#define HbParallel_RWLock_UnlockRead ReleaseSRWLockShared
+#define HbParallel_RWLock_LockWrite AcquireSRWLockExclusive
+#define HbParallel_RWLock_UnlockWrite ReleaseSRWLockExclusive
+
+typedef CONDITION_VARIABLE HbParallel_CondEvent;
+#define HbParallel_CondEvent_Init InitializeConditionVariable
+#define HbParallel_CondEvent_Destroy(condEvent) {}
+#define HbParallel_CondEvent_Await(condEvent, mutex) SleepConditionVariableCS(condEvent, mutex, INFINITE)
+#define HbParallel_CondEvent_Signal WakeConditionVariable
+#define HbParallel_CondEvent_SignalAll WakeAllConditionVariable
+
+typedef HANDLE HbParallel_Thread;
+typedef void (* HbParallel_Thread_Entry)(void * data);
+HbBool HbParallel_Thread_Start(HbParallel_Thread * thread, char const * name, HbParallel_Thread_Entry entry, void * data);
+HbForceInline void HbParallel_Thread_Destroy(HbParallel_Thread * thread) {
+	WaitForSingleObject(*thread, INFINITE);
+	CloseHandle(*thread);
+}
+
+#else
+#error No parallelization API implementation for the target OS.
+#endif
+
+#endif
