47cf164e88bce66ae2bf3f8b4bd6fa50a05abfde
Triang3l <hwguy.siplus@gmail.com>
2019-05-02 22:13:32 +0300

    Input: Gamepad stick deadzone handling

--- HbInput.c
+++ HbInput.c
@@ -10,3 +10,20 @@ HbInput_Gamepad const * HbInput_Gamepad_GetByHandle(uint32_t handle) {
 	}
 	return HbNull;
 }
+
+void HbInput_Gamepad_ApplyDeadZones(float * right, float * up, float innerDeadZoneHalfWidth, float outerDeadZoneRadius) {
+	float rightValue = *right, upValue = *up;
+	float innerDeadZoneScale = 1.0f / (1.0f - innerDeadZoneHalfWidth);
+	// Remap the magnitude limit to post-inner-deadzone coordinates.
+	float outerDeadZoneScale = 1.0f / ((outerDeadZoneRadius - innerDeadZoneHalfWidth) * innerDeadZoneScale);
+	float rightAbs = fmaxf((fabsf(rightValue) - innerDeadZoneHalfWidth) * innerDeadZoneScale, 0.0f) * outerDeadZoneScale;
+	float upAbs = fmaxf((fabsf(upValue) - innerDeadZoneHalfWidth) * innerDeadZoneScale, 0.0f) * outerDeadZoneScale;
+	float magnitudeSqr = rightAbs * rightAbs + upAbs * upAbs;
+	if (magnitudeSqr > 1.0f) {
+		float invMagnitude = 1.0f / sqrtf(magnitudeSqr);
+		rightAbs *= invMagnitude;
+		upAbs *= invMagnitude;
+	}
+	*right = copysignf(rightAbs, rightValue);
+	*up = copysignf(upAbs, upValue);
+}
--- HbInput.h
+++ HbInput.h
@@ -21,7 +21,7 @@ HbBool HbInput_Windows_HandleMessage(HWND window, UINT message, WPARAM wParam, L
 // When a gamepad is connected (or reconnected), it's given a handle that has never been used previously (monotonically increasing value).
 // This way player input can be linked directly to a specific gamepad, disregarding behind-the-scenes enumeration details.
 
-#define HbInput_Gamepad_InvalidHandle 0
+#define HbInput_Gamepad_InvalidHandle 0 // Must be zero for auto-initialization of static variables.
 
 typedef struct HbInput_Gamepad {
 	uint32_t handle;
@@ -43,6 +43,9 @@ HbInput_Gamepad const * HbInput_Gamepad_GetByIndex(uint32_t index); // Keep the 
 // The returned state is valid until the next update.
 // Implemented in common, not per-OS, code.
 HbInput_Gamepad const * HbInput_Gamepad_GetByHandle(uint32_t handle);
+// Inner dead zone is applied to each axis separately, so moving fully in one direction is possible.
+// Outer dead zone is applied after the inner one (but is in pre-inner-deadzone coordinates), the maximum magnitude, will be treated as 1.
+void HbInput_Gamepad_ApplyDeadZones(float * right, float * up, float innerDeadZoneHalfWidth, float outerDeadZoneRadius);
 
 #if HbPlatform_OS_WindowsDesktop
 // Windows.Gaming.Input is used by default (after HbInput_Init), but XInput 1.4 can be enabled to support things like DLL overrides.
